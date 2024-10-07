39fb3b634c2e3674ec853eb1c676698ebf15da77
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2024-10-06 21:02:55 -0400

    Adjust app chunking for node modules

--- mss-app/vite.config.ts
+++ mss-app/vite.config.ts
@@ -14,6 +14,15 @@ export default defineConfig({
     }
   },
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Create a separate vendor chunk for node_modules
+          }
+        }
+      }
+    }
   }
 });
