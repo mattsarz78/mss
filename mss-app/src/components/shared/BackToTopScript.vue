e948ddc9e1b38cc91df284bdd68e1cab0769ea6f
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-03-24 21:44:27 -0400

    Refactor BackToTopScript

--- mss-app/src/components/shared/BackToTopScript.vue
+++ mss-app/src/components/shared/BackToTopScript.vue
@@ -1,8 +1,24 @@
 <template>
-  <component :is="'script'">
-    window.onscroll = function() {scrollFunction()}; function scrollFunction() { const backToTopBtn =
-    document.getElementById("backToTopBtn"); if (document.body.scrollTop > 20 || document.documentElement.scrollTop >
-    20) { backToTopBtn?.classList?.add("show"); } else { backToTopBtn?.classList?.remove("show"); } } function scrollToTop()
-    { document.body.scrollTop = 0; document.documentElement.scrollTop = 0; }
-  </component>
+  <div />
 </template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+
+const scrollFunction = () => {
+  const backToTopBtn = document.getElementById('backToTopBtn');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn?.classList?.add('show');
+  } else {
+    backToTopBtn?.classList?.remove('show');
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', scrollFunction);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', scrollFunction);
+});
+</script>
