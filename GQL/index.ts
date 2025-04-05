100ecd756951dc8be11c37bda56867232f4b50db
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-04-05 18:34:14 -0400

    Add CORS

--- GQL/index.ts
+++ GQL/index.ts
@@ -17,9 +17,24 @@ import { WeeklyDatesService, WeeklyDatesServiceKey } from './database/weeklyDate
 import { CommonService, CommonServiceKey } from './database/common';
 import { RsnListService, RsnListServiceKey } from './database/rsnList';
 
+const VALID_CORS_ORIGINS = process.env.VALID_CORS_ORIGINS?.split(',') ?? [];
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+
 const app = express();
 
-app.use(cors({ origin: '*' }));
+const corsOptions: cors.CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void): void => {
+    if (isValidCors(origin) || origin === undefined) {
+      console.log('CORS allowed for origin:', origin);
+      callback(null, true);
+      return;
+    }
+    callback(new Error('Not allowed by CORS'));
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.get('/health', (req, res) => {
@@ -79,3 +94,19 @@ startServer().catch((error: unknown) => {
   console.error('Error starting server:', (error as Error).message);
   process.exit(1);
 });
+
+const isValidCors: (origin: string | undefined) => boolean = (origin) => {
+  if (NODE_ENV === 'production') {
+    return (
+      !!origin &&
+      Array.isArray(VALID_CORS_ORIGINS) &&
+      VALID_CORS_ORIGINS.some((domain) => {
+        const regexPattern = `^${domain.replace(/\\/g, '\\\\').replace(/\*/g, '[a-z0-9-]*').replace(/\./g, '\\.')}$`;
+        const regex = new RegExp(regexPattern);
+        return regex.test(origin);
+      })
+    );
+  } else {
+    return true;
+  }
+};
