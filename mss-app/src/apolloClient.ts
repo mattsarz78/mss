25c1875e88b175dabfd2576f8ae55a4b5df2887c
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-04-20 12:36:11 -0400

    Apollo Client Dev Tools

--- mss-app/src/apolloClient.ts
+++ mss-app/src/apolloClient.ts
@@ -1,4 +1,5 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
+import { connectApolloClientToVSCodeDevTools } from '@apollo/client-devtools-vscode';
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
@@ -10,3 +11,12 @@ const cache = new InMemoryCache();
 
 // Create and export the Apollo Client
 export const apolloClient = new ApolloClient({ link: httpLink, cache });
+
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const devtoolsRegistration = connectApolloClientToVSCodeDevTools(
+    apolloClient,
+    // the default port of the VSCode DevTools is 7095
+    'ws://localhost:7095'
+  );
+}
