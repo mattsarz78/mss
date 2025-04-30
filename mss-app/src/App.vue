dc0da9246598525184074ed966385090ff3dec5d
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-04-29 21:40:28 -0400

    Move ads back to index

--- mss-app/src/App.vue
+++ mss-app/src/App.vue
@@ -1,27 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { useAppUtils } from './composables/useAppUtils';
-import { defineAsyncComponent } from 'vue';
 
 const { isOnline } = useAppUtils();
-
-const AdsByGoogle = defineAsyncComponent(() => import('./adsByGoogle.vue'));
 </script>
 
 <template>
   <div>
     <div v-if="!isOnline" class="offline-banner">You are currently offline</div>
     <RouterView />
-    <div class="DONTPrint">
-      <ins
-        class="adsbygoogle"
-        style="display: block"
-        data-ad-client="ca-pub-0296554708545211"
-        data-ad-slot="9539391470"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <AdsByGoogle />
-    </div>
   </div>
 </template>
 
--- mss-app/src/adsByGoogle.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <component :is="'script'"> (adsbygoogle = window.adsbygoogle || []).push({}); </component>
-</template>
