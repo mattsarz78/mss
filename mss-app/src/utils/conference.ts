cb627993fce2d70c11f1ed649cf65fc5a6f5f333
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-05-18 09:07:02 -0400

    Consolidate contract data

--- mss-app/src/utils/conference.ts
+++ mss-app/src/utils/conference.ts
@@ -1,11 +1,11 @@
 import type { ConferenceData } from '@/staticData/exportTypes';
-import { contractData } from '../staticData/conference-data/contractData';
+import contractData from '@/staticData/conference-data/contractData.json';
 
 export const getConferenceContractData = (conference: string, season: string) => {
   return (
     contractData
       .find((contract) => contract.season === season)
-      ?.conferenceData.find((data: ConferenceData) => data.id === conference)?.data ??
+      ?.data.find((seasonData: ConferenceData) => seasonData.id === conference)?.data ??
     `Data not found for ${conference} for ${season} season`
   );
 };
