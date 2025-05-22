72e363941a167e5b27dc8659b642fa7007558665
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-05-22 10:24:28 -0400

    Remove function for navbar

--- mss-app/src/utils/navBar.ts
+++
@@ -1,8 +0,0 @@
-export const adjustNavBar = (): void => {
-  const navbar = document.getElementsByClassName('navbar')[0];
-  const main = document.getElementById('Main');
-  if (main) {
-    const paddingAddition = navbar.clientHeight - 7;
-    main.style.paddingTop = `${paddingAddition.toString()}px`;
-  }
-};
