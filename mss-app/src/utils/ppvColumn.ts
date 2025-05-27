5c5099317a8037f7d547db60911d143ad074278d
mattsarz78 <mattsarz78@users.noreply.github.com>
2025-05-26 21:26:02 -0400

    Remove unused file (#359)

--- mss-app/src/utils/ppvColumn.ts
+++
@@ -1,6 +0,0 @@
-import type { ValidSportYear } from '@/staticData/exportTypes';
-import validSportYears from '@/staticData/validSportYears.json';
-
-export const shouldShowPpvColumn = (year: string): boolean => {
-  return validSportYears.find((x: ValidSportYear) => x.season === year)?.showPPVColumn ?? false;
-};
