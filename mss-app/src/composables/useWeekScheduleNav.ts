2b0b925421b35909fb8271db818b0ea248ddce8b
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-05-29 08:42:53 -0400

    remove duplicate  functions

--- mss-app/src/composables/useWeekScheduleNav.ts
+++ mss-app/src/composables/useWeekScheduleNav.ts
@@ -6,16 +6,6 @@ const isFirstWeek = (contents: WeekInfo[], week: number): boolean => {
   return contents[0].week === week;
 };
 
-const isNextWeekBasketballPostseason = (sport: string, contents: WeekInfo[], week: number): boolean => {
-  const nextWeek = week + 1;
-  return sport === 'basketball' && contents.some((x) => x.week === nextWeek && x.postseasonInd);
-};
-
-const isNextWeekBowlGameWeek = (sport: string, contents: WeekInfo[], week: number): boolean => {
-  const nextWeek = week + 1;
-  return sport === 'football' && contents[contents.length - 1].week === nextWeek;
-};
-
 const isBowlGameWeek = (sport: string, contents: WeekInfo[], week: number): boolean => {
   return sport === 'football' && contents[contents.length - 1].week === week;
 };
@@ -43,12 +33,14 @@ export function useWeekScheduleNav(sport: string, year: string, week: number) {
     isBasketballPostseason(sport, seasonContentsResult.value?.seasonContents.seasonContents ?? [], week)
   );
   const isWeekOne = computed(() => isFirstWeek(seasonContentsResult.value?.seasonContents.seasonContents ?? [], week));
-  const isNextWeekMbkPostseason = computed(() =>
-    isNextWeekBasketballPostseason(sport, seasonContentsResult.value?.seasonContents.seasonContents ?? [], week)
-  );
-  const isNextWeekBowlWeek = computed(() =>
-    isNextWeekBowlGameWeek(sport, seasonContentsResult.value?.seasonContents.seasonContents ?? [], week)
-  );
+  const isNextWeekMbkPostseason = computed(() => {
+    const nextWeek = week + 1;
+    return isBasketballPostseason(sport, seasonContentsResult.value?.seasonContents.seasonContents ?? [], nextWeek);
+  });
+  const isNextWeekBowlWeek = computed(() => {
+    const nextWeek = week + 1;
+    return isBowlGameWeek(sport, seasonContentsResult.value?.seasonContents.seasonContents ?? [], nextWeek);
+  });
 
   return {
     seasonContentsResult,
