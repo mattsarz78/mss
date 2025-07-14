4d7c568e2a92fb4ffc5e92fa194dbe9f2829268c
Matt Sarzyniak <matt_sarz44017@yahoo.com>
2025-07-14 16:31:24 -0400

    Fix links

--- mss-app/src/components/weeklyText/WeekTextSchedule.vue
+++ mss-app/src/components/weeklyText/WeekTextSchedule.vue
@@ -56,7 +56,7 @@ const { tvGameResult, tvGameLoading, tvGameError } = useWeekTextSchedule(sport, 
               <RouterLink :to="`/season/${sport}/${paramYear}`">Season Home</RouterLink>
             </div>
             <div class="flex-row">
-              <RouterLink class="DONTPrint" :to="`/schedule/${sport}/${paramYear}/${week}/text`">
+              <RouterLink class="DONTPrint" :to="`/schedule/${sport}/${paramYear}/${week}`">
                 Weekly Schedule
               </RouterLink>
             </div>
